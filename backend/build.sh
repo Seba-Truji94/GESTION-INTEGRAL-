a68368a551ce6304f08ccb0f2d79d90f0393f33c
SebaTrujillo94 <s.trijillo@duocuc.cl>
2026-04-10 09:28:49 -0400

    Auto-seed BD en build.sh para evitar usar Shell de pago

--- backend/build.sh
+++ backend/build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Poblar base de datos con datos iniciales (solo si está vacía)
+python seed_v2.py
